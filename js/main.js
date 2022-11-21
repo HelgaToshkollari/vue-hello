5ed92ab630ae70a63ad16ee1493ae08edb826673
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-21 16:10:10 +0100

    adds image

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ createApp({
   
   data () {
     return {
-        name: "hello ! welcome to my fist attempt in Vue.js "
+        hello: "hello ! welcome to my fist attempt in Vue.js ",
+        image: "https://64.media.tumblr.com/ce1768a3c410fb500ba9e9e7f46051eb/tumblr_psbxehg7g91vvfsp9o2_400.gifv"
     };
   }
 }).mount("#app")
