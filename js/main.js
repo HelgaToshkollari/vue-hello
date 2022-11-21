8b95501bd168cd1fbe5d832a2f06b2a5c11969e9
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-21 16:15:01 +0100

    adds some changes

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ createApp({
   
   data () {
     return {
-        hello: "hello ! welcome to my fist attempt in Vue.js ",
-        image: "https://64.media.tumblr.com/ce1768a3c410fb500ba9e9e7f46051eb/tumblr_psbxehg7g91vvfsp9o2_400.gifv"
+        hello: "Hello ! take a break and have some tea !",
+        image1: "https://64.media.tumblr.com/ce1768a3c410fb500ba9e9e7f46051eb/tumblr_psbxehg7g91vvfsp9o2_400.gifv",
+        image2: "https://64.media.tumblr.com/9499b8f6c9e65b0119d193837a23d07e/tumblr_psbxehg7g91vvfsp9o1_400.gifv"
     };
   }
 }).mount("#app")
